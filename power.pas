37ce8108c5108bd1b1167ed04a5bdd37238628be
khangkhyc <108248578+khangkhyc@users.noreply.github.com>
2023-02-23 19:50:43 +0700

    a^b in O(logn)

---
+++ power.pas
@@ -0,0 +1,15 @@
+uses crt;
+var a,b:longint;
+function pow(x,n:byte):qword;
+var i:qword;
+begin
+    if n = 0 then pow := 1
+    else if n mod 2 = 0 then begin i := pow(x, n div 2); pow := i*i; end
+    else begin i := pow(x, n div 2); pow := i*i*x; end;
+end;
+begin
+    clrscr;
+    readln(a,b);
+    writeln(pow(a,b));
+    readln;
+end.
